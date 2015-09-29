e65b59d2d581c9884fc4bf3cf3174dc2ae6af518
Ajshawit <ajshawit@gmail.com>
2015-09-29 17:51:09 +1000

    Fixing active links in nav bar
    They selected correctly based on current scroll position but didn’t
    lose class upon moving to another section.

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,3 @@
-$('a[href^="#"]').on('click', function(event) {
-    var target = $(this.href);
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: target.offset().top
-        }, 1000);
-    }
-});
-
 $(window).scroll(function() {
     var y_scroll_pos = $(document).scrollTop() + 45
     var home = $( "#home" ).position()
@@ -15,21 +5,22 @@ $(window).scroll(function() {
     var portfolio = $( "#portfolio" ).position()
 	// set to whatever you want it to be
 
-	console.log(aboutme.top)
-	console.log(portfolio.top)
-	console.log(y_scroll_pos)
-
     if (y_scroll_pos < aboutme.top) {
     	$("#nav").removeClass().addClass("greenNav");
-        $("#homeLink").removeClass().addClass("active");
+        $("#aboutMeLink, #homeLink, #portfolioLink").removeClass();
+        $("#homeLink").addClass("active");
 
     }
     else if (y_scroll_pos < portfolio.top) {
     	$("#nav").removeClass().addClass("whiteNav");
-        $("#aboutMeLink").removeClass().addClass("active");
+        $("#aboutMeLink, #homeLink, #portfolioLink").removeClass();
+        $("#aboutMeLink").addClass("active");
     }	
 	else {
-		$("#nav").removeClass().addClass("blueNav");	
-        $("#portfolioLink").removeClass().addClass("active");
+		$("#nav").removeClass().addClass("blueNav");
+        $("#aboutMeLink, #homeLink, #portfolioLink").removeClass();
+        $("#portfolioLink").addClass("active");
+
 	}
 });
+
