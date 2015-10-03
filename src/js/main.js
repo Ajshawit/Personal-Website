6a3c82e8744d6e4c4c903268ff5a746f568b906e
Ajshawit <ajshawit@gmail.com>
2015-10-03 21:54:12 +1000

    Cleaning Sass and changing colours

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,24 @@
 $(window).scroll(function() {
-    var y_scroll_pos = $(document).scrollTop() + 45
-    var home = $( "#home" ).position()
-    var aboutme = $( "#aboutMe" ).position()
-    var portfolio = $( "#portfolio" ).position()
+    var y_scroll_pos = $(document).scrollTop() + 45;
+    var aboutme = $( "#aboutMe" ).position();
+    var portfolio = $( "#portfolio" ).position();
 	// set to whatever you want it to be
 
     if (y_scroll_pos < aboutme.top) {
-    	$("#nav").removeClass().addClass("greenNav");
+    	$("#nav").removeClass().addClass("homeNav");
         $("#aboutMeLink, #homeLink, #portfolioLink").removeClass();
         $("#homeLink").addClass("active");
 
     }
     else if (y_scroll_pos < portfolio.top) {
-    	$("#nav").removeClass().addClass("whiteNav");
+    	$("#nav").removeClass().addClass("aboutMeNav");
         $("#aboutMeLink, #homeLink, #portfolioLink").removeClass();
         $("#aboutMeLink").addClass("active");
     }	
 	else {
-		$("#nav").removeClass().addClass("blueNav");
+		$("#nav").removeClass().addClass("portfolioNav");
         $("#aboutMeLink, #homeLink, #portfolioLink").removeClass();
         $("#portfolioLink").addClass("active");
-
 	}
 });
 
@@ -34,7 +32,7 @@ $.fn.scrollView = function () {
       scrollTop: $(this).offset().top
     }, 500);
   });
-}
+};
 
 //Scroll to About Me
 
@@ -65,7 +63,7 @@ $( window ).scroll(function(){
         $(".portfolioCol").removeClass("col-xs-4");
     }
     else {
-        $(".portfolioCol").removeClass().addClass("portfolioCol col-md-4 col-xs-4")
+        $(".portfolioCol").removeClass().addClass("portfolioCol col-md-4 col-xs-4");
     }
 });
 
