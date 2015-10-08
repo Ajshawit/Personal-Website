14da7af7c064335f9fba070026c2cef9e5546b02
Ajshawit <ajshawit@gmail.com>
2015-10-08 14:10:24 +1100

    Readded grunt for systems not running codekit

---
+++ gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+	//Configure task(s)
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		watch : {
+			css: {
+				files: ['src/scss/*.scss', 'src/js/*.js'],
+				tasks: ['sass:dev', 'browserSync:dev', 'uglify:dev']
+			},
+		},
+		sass: {
+			dev: {
+				options: {
+					outputStyle: 'expanded'
+				},
+				files: {
+					'dist/css/main.css' : 'src/scss/main.scss'
+				}
+			},
+			dist: {
+				options: {
+					outputStyle: 'compressed'
+				},
+				files: {
+					'dist/css/main.css' : 'src/scss/main.scss'
+				}
+			},
+		},
+		browserSync: {
+		    dev: {
+		        bsFiles: {
+		            src : 'dist/css/style.css'
+		        },
+		        options: {
+		        	watchTask: true,
+		            browser: "firefox",		            
+		        }
+		    },
+		},
+		uglify: {
+		    dev: {
+		      	options: {
+		        	beautify: true
+		      	},
+		      	files: {
+		        	'dist/js/main.min.js': ['src/js/main.js']
+		     	 },
+    		},
+    		dist: {
+		      	options: {
+		        	mangle: true
+		      	},
+		      	files: {
+		        	'dist/js/main.min.js': ['src/js/main.js']
+		     	},
+    		}
+		}
+	});
+
+	//Load the plugins
+	grunt.loadNpmTasks ('grunt-contrib-watch' );
+	grunt.loadNpmTasks ('grunt-sass' );
+	grunt.loadNpmTasks ('grunt-browser-sync')
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	//Register task(s).
+	grunt.registerTask('default', ['sass:dev','uglify:dev']);
+	grunt.registerTask('dist', ['sass:dist','uglify:dist']);
+};
